0714296c82c559a9c73814adf12259b6350e6ffa
ticnoo <ticnoo@users.noreply.github.com>
2023-03-06 20:15:30 +0300

    Initial commit

---
+++ Debug/source/notification.d
@@ -0,0 +1,87 @@
+# FIXED
+
+source/notification.obj: ../source/notification.c
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/esm.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_esm.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_common.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/hal_stdtypes.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_selftest.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_pbist.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_stc.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_efc.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_core.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/system.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_system.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_gio.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_flash.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_tcram.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_vim.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_vim.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/adc.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_adc.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/can.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_can.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/mibspi.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_mibspi.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/het.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_het.h
+source/notification.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/htu.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_htu.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/gio.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/rti.h
+source/notification.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_rti.h
+
+../source/notification.c: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/esm.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_esm.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_common.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/hal_stdtypes.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_selftest.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_pbist.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_stc.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_efc.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_core.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/system.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_system.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_gio.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_flash.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_tcram.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_vim.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_vim.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/adc.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_adc.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/can.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_can.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/mibspi.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_mibspi.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/het.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_het.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/htu.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_htu.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/gio.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/rti.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_rti.h: 
---
+++ Debug/source/rti.d
@@ -0,0 +1,39 @@
+# FIXED
+
+source/rti.obj: ../source/rti.c
+source/rti.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/rti.h
+source/rti.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_rti.h
+source/rti.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_common.h
+source/rti.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/hal_stdtypes.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+source/rti.obj: C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h
+source/rti.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_vim.h
+source/rti.obj: C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_vim.h
+
+../source/rti.c: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/rti.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_rti.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_common.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/hal_stdtypes.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h: 
+C:/ccs10/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/sys_vim.h: 
+C:/Users/ticno/Desktop/ccs10\ workspace/570\ led\ blink\ 1/include/reg_vim.h: 
